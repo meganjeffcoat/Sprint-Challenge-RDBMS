99e141ff55ae7122ce4df9e9cc22f61cc27eb916
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-22 11:52:13 -0600

    small change made

--- seeds/002-actions.js
+++ seeds/002-actions.js
@@ -6,10 +6,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
 
       return knex('actions').insert([
-        {description: "Pick new book", notes: "Something about tech", completed: 0, project_id: 1},
-        {description: "Workouts", notes: "weight-training, cardio, yoga", completed: 0, project_id: 2},
-        {description: "Get gym bag ready", notes: "wash gear and pack headphones", completed: 0, project_id: 2},
-        {description: "Grab case of Topo-Chico water", notes: "20 pack from sam's", completed: 1, project_id: 3},
+        {description: "Pick new book", notes: "Something about tech", completed: 0, project_id: 7},
+        {description: "Workouts", notes: "weight-training, cardio, yoga", completed: 0, project_id: 8},
+        {description: "Get gym bag ready", notes: "wash gear and pack headphones", completed: 0, project_id: 8},
+        {description: "Grab case of Topo-Chico water", notes: "20 pack from sam's", completed: 1, project_id: 9},
       ]);
     });
 };
